cd2aa62f9535c98ec9c4a93d9ae401a64ff2c159
Ngok Chao Ho <ngokchaoho@Ngoks-MacBook-Pro.local>
2019-03-05 02:41:12 +0800

    code update

--- SourceCode/author_topic_model.cpp
+++ SourceCode/author_topic_model.cpp
@@ -11,6 +11,7 @@ public:
   AuthorTopic(double a, double b, int t, int loop, int show_limit){
     this->alpha = a;
     this->beta = b;
+	//topic size
     this->t = t;
     this->loop_count = loop;
     this->show_limit = show_limit;
@@ -18,16 +19,19 @@ public:
   }
 
   int set_author(string author){
+	//scan the current element in _authors and return its id if found
     for(int i = 0; i < _authors.size(); ++i){
       if(_authors.at(i) == author){
 	return i;
       }
     }
     _authors.push_back(author);
+	//return unique author id
     return _authors.size() - 1;
   }
 
   int set_word(string word){
+	//similar to set_author
     for(int i = 0; i < _words.size(); ++i){
       if(_words.at(i) == word){
 	return i;
@@ -37,46 +41,54 @@ public:
     return _words.size() - 1;
   }
 
-  double set_document(vector<string> authors, vector<string> words){
+  void set_document(vector<string> authors, vector<string> words){
     vector<int> word_ids;
     vector<int> author_ids;
     // set author
     for(vector<string>::iterator i = authors.begin(); i != authors.end(); ++i){
+	  // point to one author string
       string author = *i;
+	  //get a unique author id
       int author_id = set_author(author);
       author_ids.push_back(author_id);
     }
+	//vector of author_ids, each element is vector<int> for author id of one document
     _author_ids.push_back(author_ids);
 
     // set word
     for(vector<string>::iterator i = words.begin(); i != words.end(); ++i){
+	  // point to one word string
       int word_id = set_word(*i);
       word_ids.push_back(word_id);
     }
+    //vector, each element is vector of word_id
     _documents.push_back(word_ids);
 
-    // set random authorss
+    // set random authors
     vector<int> hidden_authors;
     // set random topics
     vector<int> topics;
 
     for(int i = 0; i < word_ids.size(); ++i){
       int word_id = word_ids.at(i);
+      // select a random number from 0 to topic_size - 1
       int init_topic = rand() % this->t;
-      int random_author_pos = rand() % author_ids.size();
-      int random_author = author_ids.at(random_author_pos);
+      int random_author = rand() % author_ids.size();
 
-      // inclement
+      // increment
       ++_c_at[make_pair(random_author, init_topic)];
       ++_c_wt[make_pair(word_id, init_topic)];
       _sum_c_wt[init_topic]++;
       _sum_c_at[random_author]++;
       
-      // push
+      // the topics sequence record for this document
       topics.push_back(init_topic);
+      // the author sequence record for this document
       hidden_authors.push_back(random_author);
     }
+    //the topics sequence record for all documents
     _topics.push_back(topics);
+    //the authors sequence record for all documents
     _hidden_authors.push_back(hidden_authors);
   }
 
@@ -93,7 +105,7 @@ public:
     if(_c_at.find(make_pair(author_id, topic_id)) != _c_at.end()){
       c_at_count = _c_at[make_pair(author_id, topic_id)];
     }
-    
+    //formula (5) and (6)
     double prob = (c_wt_count + this->beta) / (_sum_c_wt[topic_id] + v * this -> beta);
     prob *= (c_at_count + this->alpha) / (_sum_c_at[author_id] + this->t * this -> alpha);
 
@@ -104,34 +116,40 @@ public:
     int word_id = (_documents.at(pos_doc)).at(pos_word);
     int prev_topic = (_topics.at(pos_doc)).at(pos_word);
     int prev_author = (_hidden_authors.at(pos_doc)).at(pos_word);
+    //authors for this document
     vector<int> authors = _author_ids.at(pos_doc);
 
     // vector contains prob density
     // image
     //  |------t_1-----|---t_2---|-t_3-|------t_4-----|
     // 0.0                ^^                         1.0
+    // CDF for a word assigned to a author topic pair
     vector<double> prob;
+    //
     vector<pair<int, int> > topic_author_pairs;
 
-    // declement
+    // decrement as new topic will be assigned
     --_c_wt[make_pair(word_id, prev_topic)];
     --_sum_c_wt[prev_topic];
 
     // sum all topic/author combinations
+    // regarding 5 and 6
     for(int j = 0; j < authors.size(); ++j){
       int author_id = authors.at(j);
 
-      // declement temporally
+      // temporary decrease
       --_c_at[make_pair(author_id, prev_topic)];
       --_sum_c_at[author_id];
 
       for(int topic_id = 0; topic_id < this->t; ++topic_id){
-	double now_prob = sampling_prob(author_id, word_id, topic_id);
-	prob.push_back(now_prob);
-	if(prob.size() > 1){
-	  prob.at(prob.size() - 1) += prob.at(prob.size() - 2);
-	}
-	topic_author_pairs.push_back(make_pair(topic_id, author_id));
+	      double now_prob = sampling_prob(author_id, word_id, topic_id);
+	      prob.push_back(now_prob);
+        //Make it a CDF
+	      if(prob.size() > 1){
+	        prob.at(prob.size() - 1) += prob.at(prob.size() - 2);
+	      }
+        //record topic author calibrated
+	      topic_author_pairs.push_back(make_pair(topic_id, author_id));
       }
 
       // recover
@@ -139,7 +157,7 @@ public:
       ++_sum_c_at[author_id];
     }
 
-    // scaling [0,  1]
+    // scaling [0,  1], as (5) and (6) may not be two independent pdf
     double sum = prob.at(prob.size() - 1);
     for(int i = 0; i < (this->t * authors.size()); ++i){
       prob.at(i) /= sum;
@@ -148,14 +166,15 @@ public:
     double pos_prob = uniform_rand();
     int new_topic = 0;
     int new_author = 0;
+    //check if > prob at 0 else use the intial condition which is both 0
     if(pos_prob > prob.at(0)){
       for(int i = 1; i < (this->t * authors.size()); ++i){
-	if((pos_prob <= prob.at(i)) && (pos_prob > prob.at(i - 1))){
-	  pair<int, int> new_elem = topic_author_pairs.at(i);
-	  new_topic = new_elem.first;
-	  new_author = new_elem.second;
-	  break;
-	}
+	      if((pos_prob <= prob.at(i)) && (pos_prob > prob.at(i - 1))){
+	        pair<int, int> new_elem = topic_author_pairs.at(i);
+	        new_topic = new_elem.first;
+	        new_author = new_elem.second;
+	        break;
+	      }
       }
     }
 
@@ -163,11 +182,11 @@ public:
     (_topics.at(pos_doc)).at(pos_word) = new_topic;
     (_hidden_authors.at(pos_doc)).at(pos_word) = new_author;
 
-    // declement
+    // decrement
     --_c_at[make_pair(prev_author, prev_topic)];
     --_sum_c_at[prev_author];
 
-    // inclement
+    // increment
     ++_c_wt[make_pair(word_id, new_topic)];
     ++_sum_c_wt[new_topic];
     ++_c_at[make_pair(new_author, new_topic)];
@@ -175,14 +194,19 @@ public:
   }
 
   void sampling_all(){
+    //giving expected_count as input
     boost::progress_display progress( this->loop_count * this->_documents.size() );
+    //show total elapsed time
     boost::progress_timer cpu_time;
+    //i:iterator for required iteration
     for(int i = 0; i < this->loop_count; ++i){
+      //pos_doc:iterator looping through all documents
       for(int pos_doc = 0; pos_doc <  _documents.size(); ++pos_doc){
-	for(int pos_word = 1; pos_word < (_documents.at(pos_doc)).size(); ++pos_word){
-	  sampling(pos_doc, pos_word);
-	}
-	++progress;
+	      for(int pos_word = 1; pos_word < (_documents.at(pos_doc)).size(); ++pos_word){
+	        sampling(pos_doc, pos_word);
+	      }
+      //Increase the progress bar, after finishing one document
+	    ++progress;
       }
     }
   }
@@ -199,18 +223,16 @@ public:
     unordered_map<key, double, myhash, myeq> all_theta;
     
     for(int author_id = 0; author_id < _authors.size(); ++author_id){
-      // ソート
+      // sort
       vector<pair<double, int> > theta;
       for(int topic_id = 0; topic_id < this->t ; ++topic_id){
-
-	int c_at_count = 0;
-	if(_c_at.find(make_pair(author_id, topic_id)) != _c_at.end()){
-	  c_at_count = _c_at[make_pair(author_id, topic_id)];
-	}
-	
-	double score = (c_at_count + this->alpha)/(_sum_c_at[author_id] + this->t * this -> alpha);
-	theta.push_back(make_pair(score, topic_id));
-	all_theta[make_pair(author_id, topic_id)] = score;
+	      int c_at_count = 0;
+	      if(_c_at.find(make_pair(author_id, topic_id)) != _c_at.end()){
+	        c_at_count = _c_at[make_pair(author_id, topic_id)];
+	      }
+	      double score = (c_at_count + this->alpha)/(_sum_c_at[author_id] + this->t * this -> alpha);
+	      theta.push_back(make_pair(score, topic_id));
+	      all_theta[make_pair(author_id, topic_id)] = score;
       }
 
       // output
@@ -218,12 +240,12 @@ public:
       vector<pair<double, int> >::reverse_iterator j;
       int count = 0;
       for(j = theta.rbegin(); j != theta.rend(); ++j){
-	if(count >= this->show_limit){
-	  break;
-	}else{
-	  ofs_theta << _authors.at(author_id) << "\t" << (*j).second << "\t" << (*j).first << endl;
-	  count++;
-	}
+	      if(count >= this->show_limit){
+	        break;
+	      } else{
+	      ofs_theta << _authors.at(author_id) << "\t" << (*j).second << "\t" << (*j).first << endl;
+	      count++;
+	      }
       }
     }
     ofs_theta.close();
@@ -238,19 +260,19 @@ public:
       // sort
       vector<pair<double, string> > phi;
       for(int word_id = 0; word_id < _words.size(); ++word_id){
-	int c_wt_count = 0;
-	if(_c_wt.find(make_pair(word_id, topic_id)) != _c_wt.end()){
-	  c_wt_count = _c_wt[make_pair(word_id, topic_id)];
-	}
-	double score = (c_wt_count + this->beta)/(_sum_c_wt[topic_id] + _words.size() * this -> beta);
-	phi.push_back(make_pair(score, _words.at(word_id)));
+	      int c_wt_count = 0;
+	      if(_c_wt.find(make_pair(word_id, topic_id)) != _c_wt.end()){
+	        c_wt_count = _c_wt[make_pair(word_id, topic_id)];
+	      }
+	    double score = (c_wt_count + this->beta)/(_sum_c_wt[topic_id] + _words.size() * this -> beta);
+	    phi.push_back(make_pair(score, _words.at(word_id)));
       }
 
       // sort all theta
       vector<pair<double, string> > topic_given_theta;
       for(int author_id = 0; author_id < _authors.size(); ++author_id){
-	double score = all_theta[make_pair(author_id, topic_id)];
-	topic_given_theta.push_back(make_pair(score, _authors.at(author_id)));
+	      double score = all_theta[make_pair(author_id, topic_id)];
+	      topic_given_theta.push_back(make_pair(score, _authors.at(author_id)));
       }
 
       // output
@@ -258,22 +280,21 @@ public:
       vector<pair<double, string> >::reverse_iterator j;
       int count = 0;
       for(j = phi.rbegin(); j != phi.rend(); ++j){
-	if(count >= this->show_limit){
-	  break;
-	}else{
-	  ofs_phi << topic_id << "\t" << (*j).second << "\t" << (*j).first << endl;
-	  count++;
-	}
+	      if(count >= this->show_limit){
+	        break;
+	      } else{
+	        ofs_phi << topic_id << "\t" << (*j).second << "\t" << (*j).first << endl;
+	        count++;
+	      }
       }
-
       sort(topic_given_theta.begin(), topic_given_theta.end());
       count = 0;
       for(j = topic_given_theta.rbegin(); j != topic_given_theta.rend(); ++j){
-	if(count >= this->show_limit){
-	  break;
-	}else{
-	  count++;
-	}
+	      if(count >= this->show_limit){
+	        break;
+	      }else{
+	        count++;
+	      }
       }
     }
     ofs_phi.close();
@@ -282,6 +303,8 @@ public:
 private:
   // key: <word_id, topic_id>
   // value: # of assign
+  // http://www.cplusplus.com/reference/unordered_map/unordered_map/
+  // Parameter key, T(return type), hash function, Pred
   unordered_map<key, int, myhash, myeq> _c_wt;
   
   // key: <author_id, topic_id>
--- SourceCode/tool.cpp
+++ SourceCode/tool.cpp
@@ -11,13 +11,13 @@
 #include <time.h>
 
 typedef std::pair<int, int> key;
-
+//Functor for comparing pair<int,int>
 struct myeq{
   bool operator() (const std::pair<int, int> & x, const std::pair<int, int> & y) const{
     return x.first == y.first && x.second == y.second;
   }
 };
-
+//Functor for Hash function
 struct myhash{
 private:
   const std::hash<int> h_int;
@@ -28,7 +28,7 @@ public:
     return h_int(p.second) + 0x9e3779b9 + (seed<<6) + (seed>>2);
   }
 };
-
+//Function for splitting string
 std::vector<std::string> split_string(std::string s, std::string c){
   std::vector<std::string> ret;
   for(int i = 0, n = 0; i <= s.length(); i = n + 1){
@@ -39,7 +39,7 @@ std::vector<std::string> split_string(std::string s, std::string c){
   }
   return ret;
 }
-
+//pdf of uniformly distributed random number
 double uniform_rand(){
   return (double)rand() * (1.0 / (RAND_MAX + 1.0));
 }
